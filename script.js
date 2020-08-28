9c66e53cce761089b4ef197ee608d3debcc4673d
St3DiTwister <56047874+St3DiTwister@users.noreply.github.com>
2020-08-29 00:43:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,16 +7,7 @@ function input(i) {
 
 function input2(i) {
     let y = inputCalc.value.substr(inputCalc.value.length - 1);
-    if (y === "%" || y === "/" || y === "*" || y === "-" || y === "+") {
-        //pass
-    }
-    else {
-        inputCalc.value = inputCalc.value + i
-    }
-}
-
-function input3(i) {
-    if (inputCalc.value.includes(".")) {
+    if (y === "%" || y === "/" || y === "*" || y === "-" || y === "+" || y === ".") {
         //pass
     }
     else {
